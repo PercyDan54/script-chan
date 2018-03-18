bf667a361ea758b4b430cf35d60f3ebee7aab4af
shARPII <sharpiianimeultime@gmail.com>
2018-03-18 01:37:09 +0100

    disable switch command if you have not referenced a private IP address

--- osu!tournament/Rooms/Ranking/TeamVs/ViewModels/MultiplayerCommandsViewModel.cs
+++ osu!tournament/Rooms/Ranking/TeamVs/ViewModels/MultiplayerCommandsViewModel.cs
@@ -9,17 +9,35 @@ using System.Threading.Tasks;
 using Osu.Ircbot;
 using Osu.Scores;
 using Osu.Utils;
+using Osu.Utils.Info;
 
 namespace Osu.Mvvm.Rooms.Ranking.TeamVs.ViewModels
 {
     public class MultiplayerCommandsViewModel : Screen, IRankingViewModel
     {
+        #region Attributes
+        /// <summary>
+        /// The room
+        /// </summary>
         public Room room;
 
+        /// <summary>
+        /// The blue team name
+        /// </summary>
         private string BlueTeamName { get; set; }
-        private string RedTeamName { get; set; }
-        private string GroupName { get; set; }
 
+        /// <summary>
+        /// The red team name
+        /// </summary>
+        private string RedTeamName { get; set; }
+
+        /// <summary>
+        /// The group name in case we are not in the TeamVs situation
+        /// </summary>
+        private string GroupName { get; set; }
+        #endregion
+
+        #region Constructors
         public MultiplayerCommandsViewModel(Room r, string bTeamname, string rTeamName) : this(r)
         {
             BlueTeamName = bTeamname;
@@ -35,29 +53,59 @@ namespace Osu.Mvvm.Rooms.Ranking.TeamVs.ViewModels
         {
             room = r;
         }
+        #endregion
 
+        #region Properties
+        /// <summary>
+        /// IsOsuAdmin property to know if we have to enable the 'switch server' button
+        /// </summary>
+        public bool IsOsuAdmin
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(InfosHelper.UserDataInfos.IPPrivateBancho);
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Send mp start 5 to the osu room
+        /// </summary>
         public void SendStart()
         {
             OsuIrcBot.GetInstancePrivate().SendMessage("#mp_" + room.Id, "!mp start 5");
         }
 
+        /// <summary>
+        /// Send mp settings to the osu room
+        /// </summary>
         public void SendSettings()
         {
             room.Ranking.GetDiscordStatus();
             OsuIrcBot.GetInstancePrivate().SendMessage("#mp_" + room.Id, "!mp settings");
         }
 
+        /// <summary>
+        /// Change the password of the osu room
+        /// </summary>
         public async void SetPassword()
         {
             var pw = await Dialog.ShowInput("New password", "Enter the new password for the room");
             OsuIrcBot.GetInstancePrivate().SendMessage("#mp_" + room.Id, "!mp password " + pw);
         }
 
+        /// <summary>
+        /// Send the welcome message generated for the osu room
+        /// </summary>
         public void SendWelcomeMessage()
         {
             OsuIrcBot.GetInstancePrivate().SendWelcomeMessage(room);
         }
 
+        /// <summary>
+        /// Send switch commands on osu to switch every players referenced in both teams or group
+        /// </summary>
         public async void SendQuickSwitch()
         {
             if(string.IsNullOrEmpty(GroupName))
@@ -71,6 +119,9 @@ namespace Osu.Mvvm.Rooms.Ranking.TeamVs.ViewModels
             }
         }
 
+        /// <summary>
+        /// Send invite commands on osu to invite every players referenced in both teams or group
+        /// </summary>
         public async void SendInvitations()
         {
             if (string.IsNullOrEmpty(GroupName))
@@ -84,10 +135,20 @@ namespace Osu.Mvvm.Rooms.Ranking.TeamVs.ViewModels
             }
         }
 
+        /// <summary>
+        /// Update the view model
+        /// </summary>
         public void Update()
         {
         }
+        #endregion
 
+        #region Private Methods
+        /// <summary>
+        /// Switch a team to the private server by sending switch command
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         private async Task Switch(string name)
         {
             SwitchHandler sh = new SwitchHandler();
@@ -106,6 +167,11 @@ namespace Osu.Mvvm.Rooms.Ranking.TeamVs.ViewModels
             }
         }
 
+        /// <summary>
+        /// Invite a team to the room by sending invite command
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         private async Task Invite(string name)
         {
             SwitchHandler sh = new SwitchHandler();
@@ -123,5 +189,6 @@ namespace Osu.Mvvm.Rooms.Ranking.TeamVs.ViewModels
                 }
             }
         }
+        #endregion
     }
 }
