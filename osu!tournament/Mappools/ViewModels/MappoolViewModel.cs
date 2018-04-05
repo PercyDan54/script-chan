1ba3cbaf7c885f5ba35005e0b2bf1ef7d9ab6413
Borengar <kramer.martin.km@gmail.com>
2018-04-05 20:19:49 +0200

    Add new beatmaps as nomod

--- osu!tournament/Mappools/ViewModels/MappoolViewModel.cs
+++ osu!tournament/Mappools/ViewModels/MappoolViewModel.cs
@@ -127,6 +127,7 @@ namespace Osu.Mvvm.Mappools.ViewModels
                                     // Change its osu!beatmap
                                     beatmap.OsuBeatmap = osu_beatmap;
                                     beatmap.Id = osu_beatmap.BeatmapID;
+                                    beatmap.AddMod(PickType.None);
 
                                     // Add this beatmap to the mappool list
                                     mappool.Pool[osu_beatmap.BeatmapID] = beatmap;
