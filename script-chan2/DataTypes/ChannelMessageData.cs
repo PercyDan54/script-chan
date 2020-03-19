d1262c26d0886352d070917160ae92d7e57a2f86
Borengar <kramer.martin.km@gmail.com>
2020-03-19 21:30:25 +0100

    Move irc classes

---
+++ script-chan2/DataTypes/ChannelMessageData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace script_chan2.DataTypes
+{
+    public class ChannelMessageData
+    {
+        public string Channel { get; set; }
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ script-chan2/DataTypes/IrcMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace script_chan2.DataTypes
+{
+    public class IrcMessage
+    {
+        public Match Match { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
