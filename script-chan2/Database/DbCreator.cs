aaa435ddcbbdcec07a6244c5a56989ad2901f0d9
Borengar <kramer.martin.km@gmail.com>
2020-04-05 18:17:18 +0200

    increment initial db version

--- script-chan2/Database/DbCreator.cs
+++ script-chan2/Database/DbCreator.cs
@@ -65,7 +65,7 @@ namespace script_chan2.Database
                 ('defaultTimerCommand', '120'),
                 ('defaultTimerAfterGame', '120'),
                 ('defaultTimerAfterPick', '120'),
-                ('dbVersion', '1')", conn))
+                ('dbVersion', '2')", conn))
             {
                 command.ExecuteNonQuery();
             }
